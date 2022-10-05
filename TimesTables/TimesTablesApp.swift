5d8c42dd1f73da1e67eed75ef1dbf82217cd5395
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-05 07:32:47 +0200

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Zaid Neurothrone on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
